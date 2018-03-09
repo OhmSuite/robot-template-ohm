248cf39adaaa590df309aad707c9e56f13c99846
Quunii <kennethmiuraubw@gmail.com>
2018-03-09 17:08:14 -0500

    initial commit of minimalistic test setup

--- Robot-Template-Ohm/src/main/java/com/team1389/robot/Robot.java
+++ Robot-Template-Ohm/src/main/java/com/team1389/robot/Robot.java
@@ -1,14 +1,7 @@
 
 package com.team1389.robot;
 
-import com.team1389.auto.AutoModeBase;
-import com.team1389.auto.AutoModeExecuter;
-import com.team1389.configuration.PIDConstants;
-import com.team1389.hardware.outputs.hardware.CANTalonHardware;
 import com.team1389.hardware.registry.Registry;
-import com.team1389.hardware.registry.port_types.CAN;
-import com.team1389.operation.TeleopMain;
-import com.team1389.watchers.DashboardInput;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 
@@ -21,58 +14,20 @@ import edu.wpi.first.wpilibj.IterativeRobot;
  */
 public class Robot extends IterativeRobot
 {
-	RobotSoftware robot;
-	TeleopMain teleOperator;
-	AutoModeExecuter autoModeExecuter;
-	CANTalonHardware talon;
-	Registry registry;
-
-	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
-	 */
+	Registry registry; 
 	@Override
 	public void robotInit()
 	{
 		registry = new Registry();
-		robot = RobotSoftware.getInstance();
-		teleOperator = new TeleopMain(robot);
-		autoModeExecuter = new AutoModeExecuter();
-		DashboardInput.getInstance().init();
-		talon = new CANTalonHardware(false, new CAN(0), registry);
-
 	}
-
-	@Override
-	public void autonomousInit()
-	{
-
-		autoModeExecuter.stop();
-		AutoModeBase selectedAutonMode = DashboardInput.getInstance().getSelectedAutonMode();
-		autoModeExecuter.setAutoMode(selectedAutonMode);
-
-	}
-
+	
 	@Override
 	public void teleopInit()
 	{
-		autoModeExecuter.stop();
-
-		teleOperator.init();
 	}
-
-	/**
-	 * This function is called periodically during operator control
-	 */
 	@Override
 	public void teleopPeriodic()
 	{
-		teleOperator.periodic();
-		talon.getVelocityControl(new PIDConstants(1, 0, 0)).set(500);
 	}
 
-	@Override
-	public void disabledInit()
-	{
-	}
 }
